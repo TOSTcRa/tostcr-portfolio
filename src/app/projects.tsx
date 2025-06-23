1fd4bb3722e5ef52103a6e4a2aa43c6d9542d65e
Andriy <antostcr@gmail.com>
2025-06-23 04:51:01 +0300

    Update projects.tsx

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -3,38 +3,12 @@ import Image from "next/image";
 import {useEffect, useState} from "react";
 import {getProjects} from "./lib/getProjects";
 export default function Projects() {
-	// const projects = [
-	// 	{
-	// 		name: "Portfolio Website",
-	// 		img: "/projects/project-1.png",
-	// 		description:
-	// 			"Responsive porfolio website with admin panel and a function to manage website pictures in the admin panel",
-	// 	},
-	// 	{
-	// 		name: "Task manager",
-	// 		img: "/projects/project-2.png",
-	// 		description:
-	// 			"A task manager website made with typescript and tailwindcss, has functions to add, remove or manage items between columns",
-	// 	},
-	// 	{
-	// 		name: "Whitepace",
-	// 		img: "/projects/project-3.png",
-	// 		description:
-	// 			"A SaaS landing page from the figma community made with SvelteKIT",
-	// 	},
-	// 	{
-	// 		name: "Product From",
-	// 		img: "/projects/project-4.png",
-	// 		description:
-	// 			"Website made on laravel and php, has functionality of managing the products added to the table",
-	// 	},
-	// ];
-
 	interface Project {
 		id: number;
 		name: string;
 		img: string;
 		description: string;
+		github: string;
 	}
 
 	const [projects, setProjects] = useState<Project[]>([]);
@@ -66,7 +40,9 @@ export default function Projects() {
 							height={300}
 						/>
 						<div className='max-w-[250px] flex flex-col items-center text-center md:items-start md:text-start'>
-							<h4>{item.name}</h4>
+							<a href={`${item.github}`} target='_blank'>
+								{item.name}
+							</a>
 							<p className='text-sm leading-5.5 text-gray-600 dark:text-gray-400'>
 								{item.description}
 							</p>
